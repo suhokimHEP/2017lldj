d96bb12bd07bd4a200a6184e5cd090a5d40744f8
Daniel Diaz <ddiaz006@ucr.edu>
2019-01-31 14:47:15 -0600

    remove OOT photons here too

--- ntuples/config/run_mc_94XAOD.py
+++ ntuples/config/run_mc_94XAOD.py
@@ -177,6 +177,12 @@ process.lldjNtuple = cms.EDAnalyzer('lldjNtuple',
 
 )
 
+# Double check: this is to remove the OOT photons, which is causing a crash
+#process.patCandidates.remove(process.patCandidateSummary)
+process.patCandidatesTask.remove(process.makePatOOTPhotonsTask)
+#process.selectedPatCandidates.remove(process.selectedPatCandidateSummary)
+process.selectedPatCandidatesTask.remove(process.selectedPatOOTPhotons)
+#process.cleanPatCandidates.remove(process.cleanPatCandidateSummary)
 
 #builds Ntuple
 process.p = cms.Path(

a0238e359de6f00ffd79e4c3b05d21ac3673b659
Daniel Diaz <ddiaz006@ucr.edu>
2019-03-01 16:41:57 -0600

    nversion name

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 export depot="/eos/uscms/store/group/lpchbb/LLDJntuples"
 export depot2="/store/group/lpchbb/LLDJntuples"
 export FWVersion="2017lldj"
-export nversion="2017TestDaniel9"
+export nversion="2017_LLDJ"
 export aversion="2017TestDanielAn9"
 
 pushd `dirname ${BASH_SOURCE[0]}` > /dev/null

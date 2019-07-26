fd0ecc3d43c86b3c3006eb803ceeafaa4b7c6cfd
Daniel Diaz <ddiaz006@ucr.edu>
2019-07-26 14:55:31 -0500

    Cleaning up Scripts/Adding summed GenEventWeights for DY and STs

--- submitters/submitjobs.sh
+++ submitters/submitjobs.sh
@@ -11,8 +11,42 @@ nevents=-1
 maxfilesperjob=200   # 500=6h
 
 samples=(  \
- "DYJetsToLL_M-50"            \
- "ST_s-channel_4f_leptonDecays"             \
+# "Data_DoubleMuon_B"         \
+# "Data_DoubleMuon_C"         \
+# "Data_DoubleMuon_D"         \
+# "Data_DoubleMuon_E"         \
+# "Data_DoubleMuon_F"         \
+# "Data_DoubleEG_B"         \
+# "Data_DoubleEG_C"         \
+# "Data_DoubleEG_D"         \
+# "Data_DoubleEG_E"         \
+# "Data_DoubleEG_F"         \
+# "Data_MuonEG_B"         \
+# "Data_MuonEG_C"         \
+# "Data_MuonEG_D"         \
+# "Data_MuonEG_E"         \
+# "Data_MuonEG_F"         \
+# "DYJetsToLL_M-50"            \
+# "WJetsToLNu"     \
+# "TTJets"          \
+# "ST_s-channel_4f_leptonDecays"             \
+# "ST_t-channel_antitop_4f_inclusiveDecays"  \
+# "ST_t-channel_top_4f_inclusiveDecays"      \
+# "ST_tW_antitop_5f_NoFullyHadronicDecays"   \
+# "ST_tW_top_5f_NoFullyHadronicDecays"        \
+# "WW"                 \
+# "WZ"                 \
+# "ZZ"                 \
+# "WGToLNuG"              \
+# "ZGTo2LG"               \
+# "ZH_HToBB_ZToLL"        \
+# "ggZH_HToBB_ZToLL"      \
+# "QCD_HT100to200"        \
+# "QCD_HT300to500"        \
+# "QCD_HT500to700"        \
+# "QCD_HT700to1000"       \
+# "QCD_HT1500to2000"      \
+# "QCD_HT2000toInf"       \
 )
 # "Data_DoubleMuon_B"         \
 # "Data_DoubleMuon_C"         \

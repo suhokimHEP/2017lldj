3ea58d355434f67b356cf096d013a47e1c5d2252
Daniel Diaz <ddiaz006@ucr.edu>
2019-10-11 18:25:25 -0500

    misc. tweaks

--- setup.sh
+++ setup.sh
@@ -3,9 +3,15 @@
 export depot="/eos/uscms/store/group/lpchbb/LLDJntuples"
 export depot2="/store/group/lpchbb/LLDJntuples"
 export FWVersion="2017lldj"
-export nversion="2017_LLDJ"
+export nversion="2017lldj_NoJEC"
 #export aversion="2017Comparisons_noPUWeights_simTrig"
-export aversion="2017Comparisons_noPUWeights"
+#export aversion="amc_Inclusive_FineBins_SF_31July"
+#export aversion="amc_kfactor_TVShift_20Aug"
+export aversion="amc_kfactor_TVShift_20Aug_UncCats"
+
+#export aversion="amcPU_MakingPlots2"
+
+#export aversion="amcPU_gettingnPU"
 
 pushd `dirname ${BASH_SOURCE[0]}` > /dev/null
 export basedir=`pwd`

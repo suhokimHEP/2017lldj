0df6c59306eafab988783b706691e1edf8877dd1
Daniel Diaz <ddiaz006@ucr.edu>
2020-01-20 11:12:42 -0600

    Updating lists to those used for AN

--- setup.sh
+++ setup.sh
@@ -4,12 +4,16 @@ export depot="/eos/uscms/store/group/lpchbb/LLDJntuples"
 export depot2="/store/group/lpchbb/LLDJntuples"
 export FWVersion="2017lldj"
 export nversion="2017lldj_NoJEC"
-#export aversion="2017Comparisons_noPUWeights_simTrig"
-#export aversion="amc_Inclusive_FineBins_SF_31July"
-#export aversion="amc_kfactor_TVShift_20Aug"
-export aversion="amc_kfactor_TVShift_20Aug_UncCats"
+#export aversion="newPUWeights_TVShift_kfactor_LooseJetCuts"
+#export aversion="newPUWeights_TVShift_kfactor_JetCuts_pt30_eta2p0"
+#export aversion="newPUWeights_TVShift_kfactor"
 
 #export aversion="amcPU_MakingPlots2"
+#export aversion="newPUWeights_TVShift_kfactor_JetCuts_eta2p0"
+export aversion="oldPUWeights_TVShift_kfactor"
+
+#export aversion="PUWeights_NoTVShift_ZSF_OPT"
+#export aversion="CorrectPUWeights_TVShift_ZSF"
 
 #export aversion="amcPU_gettingnPU"
 
